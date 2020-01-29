914c72f4fa67504e999e9e6707c5a65c5c175cfa
Imam0415 <60427493+Imam0415@users.noreply.github.com>
2020-01-29 20:21:47 +0700

    SI PT Aulia Perumahan
    Sistem informasi pengolahan jual dan olah data pt aulia perumahan

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="container">
+
+    <div class="row">
+        <div class="col-xs-10">
+            Copyright &COPY; <?= date('Y')?> <b>Ali Sabar Sagala, Fariz Ramadhan, Imam Wahyudi 15</b> |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Peta Situs</a>
+            <a href="#">Support</a>
+        </div>
+    </div>
+</div>
